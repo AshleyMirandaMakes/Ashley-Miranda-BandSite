21dedde20873096577baebfcf44486e5bcc3193b
Ashley <miranda.ash.19@gmail.com>
2021-04-27 19:50:28 -0400

    mobile index.hmtl styling done

--- sprint-two/Scripts/index-page.js
+++ sprint-two/Scripts/index-page.js
@@ -15,18 +15,21 @@ const comments = [
     comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
   }
 ]
+
 //item id selection to link to html
 const commentListEl = document.getElementById("comment__list")
 
 //check that the object array works
-const comment = comments[0];
+// const comment = comments[0];
 //console.log(comments);
 
 //this invokes my card-creating function
 // createCommentCards(comments[0]);
 // createCommentCards(comments[1]);
 // createCommentCards(comments[2]);
+// this prints my 3 cards onto the html just fine
 
+//this loop creates comment cards and attaches them to the comment list on html
  for (let i = 0; i < comments.length; i++) {
    let cardEl = createCommentCards(comments[i]);
   //  console.log(createCommentCards(comments[i]));
@@ -42,24 +45,29 @@ commentListEl.appendChild(cardEl);
 const cardImage = createElementWithClass("div", "comment__image");
 cardEl.appendChild(cardImage);
 
-const cardContentTop = createElementWithClass("div", "comment__content--top");
-cardEl.appendChild(cardContentTop);
+const cardContent = createElementWithClass("div", "comment__content");
+cardEl.appendChild(cardContent);
 
-const cardName = createElementWithClass("h2", "comment__name");
+const cardContentTop = createElementWithClass("div", "comment__content--top");
+cardContent.appendChild(cardContentTop);
+
+const cardName = createElementWithClass("h4", "comment__name");
 cardName.innerText = comment.name;
 cardContentTop.appendChild(cardName);
 
-const cardTimestamp = createElementWithClass("h2", "comment__timestamp");
+const cardTimestamp = createElementWithClass("h4", "comment__timestamp");
 cardTimestamp.innerText = comment.timestamp;
 cardContentTop.appendChild(cardTimestamp);
+//learn how to manipulate this timestamp
 
 const cardContentBottom = createElementWithClass("div", "comment__content--bottom");
-cardEl.appendChild(cardContentBottom);
+cardContent.appendChild(cardContentBottom);
 
 const cardComment = createElementWithClass("p", "comment__comment");
 cardComment.innerText = comment.comment;
 cardContentBottom.appendChild(cardComment);
 
+//don't forget your return!
 return cardEl;
 };
 
