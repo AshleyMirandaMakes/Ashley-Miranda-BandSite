280cda813b6b23fa83615764801d8c9ba6c2867f
Ashley <miranda.ash.19@gmail.com>
2021-04-27 20:33:32 -0400

    copydeck added, shows html blocked in, shows js object started

---
+++ sprint-two/Scripts/build-shows-page.js
@@ -0,0 +1,47 @@
+const shows = [
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA"
+  },
+  {
+    date: "Tue Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA"
+  },
+  {
+    date: "Fri Oct 15 2021",
+    venue: "View Lounge",
+    location: "San Francisco, CA"
+  },
+  {
+    date: "Sat Nov 06 2021",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA"
+  },
+  {
+    date: "Fri Nov 26 2021",
+    venue: "Moscow Center",
+    location: "San Francisco, CA"
+  },
+  {
+    date: "Wed Dec 15 2021",
+    venue: "Press Club",
+    location: "San Francisco, CA"
+  },
+];
+
+console.log(shows[0]);
+
+
+{/* <section class="shows__section" id="shows__list">
+       <article class="shows__card">
+         <h4 class="card__title">DATES</h4>
+         <h3 class="card__date">Sept 06 2021</h3>
+         <h4 class="card__title">VENUE</h4>
+         <h3 class="card__venue">Ronald Lane</h3>
+         <h4 class="card__title">VENUE</h4>
+         <h3 class="card__location">SF, CA</h3>
+         <a href="#" class="button">BUT TICKETS</a>
+       </article>
+      </section> */}
